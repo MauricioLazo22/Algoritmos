817c12001459246bf333f5fe9a86876d258d1a8a
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-12 15:55:04 -0500

    Creando la clase PythagorasTree.java
    Se creó la clase PythagorasTree como un JPanel que recibirá la profundidad del fractal en su constructor y definirá un área de dibujo de 800x800 píxeles. En paintComponent, se configura el contexto gráfico (Graphics2D) pintando el fondo de negro, preparando el lienzo para la implementación futura del algoritmo recursivo que dibujará el árbol fractal.

---
+++ Laboratorio-04/Actividades/PythagorasTree.java
@@ -0,0 +1,22 @@
+package Actividades;
+import javax.swing.JPanel;
+
+public class PythagorasTree {
+    private int profundidad;
+
+    public PythagorasTree(int profundidad) {
+        this.profundidad = profundidad;
+        setPreferredSize(new Dimension(800, 800));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+        g2d.setColor(Color.GREEN);
+        // Llamada inicial para la recursión
+        trazaArbol(g2d, 350, 600, 100, -90, profundidad);
+    }
+}
