27cf4e3e61b97c0dc6ced8e392ca88e1238ca168
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-12 16:05:25 -0500

    Implementación del método busquedaBinaria
    Se completó el método busquedaBinaria con el algoritmo clásico: inicializa los límites inferior (inf=0) y superior (sup=array.length-1), luego itera calculando el centro (cen) y comparando el valor en esa posición con el dato buscado. Si lo encuentra retorna el índice; si el dato es menor ajusta sup para buscar en la mitad izquierda, si es mayor ajusta inf para la derecha. Si termina el ciclo sin encontrar el dato, retorna -1. El algoritmo garantiza complejidad O(log n) para arreglos ordenados.

--- Laboratorio-04/Actividad3/BinarySearch.java
+++ Laboratorio-04/Actividad3/BinarySearch.java
@@ -6,6 +6,23 @@ public class BinarySearch {
     }
 
     public static int busquedaBinaria(int[] array, int dato) {
+        int inf = 0;
+        int sup = array.length - 1;
         
+        while (inf <= sup) {
+            int cen = inf + (sup - inf) / 2;
+            
+            if (array[cen] == dato) {
+                return cen;
+            }
+            
+            if (array[cen] > dato) {
+                sup = cen - 1;
+            } else {
+                inf = cen + 1;
+            }
+        }
+        
+        return -1;
     }
 }
