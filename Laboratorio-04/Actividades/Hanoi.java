9b6dae1969313c254a659856e1bde63122207de2
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-12 15:47:25 -0500

    Implementación de método torresHanoi
    Este método resuelve el problema clásico de las Torres de Hanoi usando recursión: si solo hay un disco (caso base), lo mueve directamente de la torre origen (torre1) a la torre destino (torre3); para más discos (dominio), traslada n-1 discos a la torre auxiliar (torre2), mueve el disco restante a la torre destino y luego repite el proceso recursivamente para los n-1 discos, intercambiando las torres auxiliar y origen. Cada movimiento se imprime en consola.

--- Laboratorio-04/Actividades/Hanoi.java
+++ Laboratorio-04/Actividades/Hanoi.java
@@ -5,5 +5,15 @@ public class Hanoi {
     public static void main(String[] args) {
         
     }
-    
+
+    //METODO RECURSIVO PARA LA RESOLUCIÓN DEL PROBLEMA DE LAS TORRES DE HANOI
+    public void torresHanoi(int discos, int torre1, int torre2, int torre3){
+        if (discos == 1){                                                                   // Caso Base
+            System.out.println("Mover disco de torre " + torre1 + " a torre " + torre3);
+        } else{                                                                             // Dominio (problema – 1)
+            torresHanoi(discos -1, torre1, torre3,torre2);
+            System.out.println("mover disco de torre "+ torre1 + " a torre " + torre3);
+            torresHanoi(discos -1, torre2, torre1, torre3);
+        }
+    }
 }
