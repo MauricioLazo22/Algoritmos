2326f67adb4063b9653cf4dd8b9852a0f57c1fc3
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-12 16:04:10 -0500

    Creando clase BinarySearch
    Se creó la clase BinarySearch con un método estático busquedaBinaria que recibirá un arreglo ordenado (int[] array) y un valor a buscar (int dato), devolviendo su índice. El main está vacío, listo para añadir pruebas.

---
+++ Laboratorio-04/Actividad3/BinarySearch.java
@@ -0,0 +1,11 @@
+package Actividad3;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int busquedaBinaria(int[] array, int dato) {
+        
+    }
+}
