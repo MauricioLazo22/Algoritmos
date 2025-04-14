81535dce987f9f67b0995af58aa7da61ee4644ad
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-13 22:25:46 -0500

    Creando la clase modaa2.java e implementación del método moda2
    Se creó la clase modaa2 con el método moda2 que calcula la moda en arreglos previamente ordenados con complejidad O(n): inicializa punteros (p y first) para recorrer el arreglo comparando elementos consecutivos, incrementando un contador de frecuencia (frec) cuando encuentra valores iguales y actualizando la moda (moda) y frecuencia máxima (maxfrec) al detectar un nuevo valor. Aunque más eficiente que la versión anterior, requiere que el arreglo esté ordenado para funcionar correctamente. El método es óptimo para este caso particular pero no maneja arreglos desordenados.

---
+++ Laboratorio-04/Actividad4/modaa2.java
@@ -0,0 +1,28 @@
+package Actividad4;
+
+public class modaa2 {
+    public static int moda2(int array[]) {
+        int first = 1;
+        int p = 0;
+        int end = array.length-1;
+        int moda = array[0];
+        int frec = 1;
+        int maxfrec = 0;
+        while (first <= end) {
+            if (array[p] == array[first]) {
+                frec ++; 
+                first++; 
+            }
+            else {
+                if (frec > maxfrec) {
+                    maxfrec = frec; 
+                    moda = array[0]; 
+                }
+                p = first; 
+                first = p+1; 
+                frec = 1; 
+            }
+        }
+        return moda;
+    }
+}
