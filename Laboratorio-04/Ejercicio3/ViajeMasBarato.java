d6eb06b5711b8bde35abf29274173805a7124c05
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-04-13 23:09:48 -0500

    Creando la clase ViajeMasBarato e implementando el método calcularCostosMinimos
    El método calcularCostosMinimos recibe una matriz de costos directos T y devuelve una matriz C con los costos mínimos entre todos los nodos, considerando posibles escalas: primero inicializa C con costos directos (superior derecha), ceros en la diagonal (mismo nodo) e infinitos en la inferior izquierda (direcciones no permitidas), luego aplica programación dinámica recorriendo la matriz desde el final hasta el inicio para calcular iterativamente los costos óptimos evaluando todas las posibles combinaciones con escalas intermedias (k) que puedan ofrecer rutas más económicas, actualizando C[i][j] solo cuando encuentra un camino válido y de menor costo que el almacenado previamente.

---
+++ Laboratorio-04/Ejercicio3/ViajeMasBarato.java
@@ -0,0 +1,32 @@
+package Ejercicio3;
+
+public class ViajeMasBarato {
+    public static int[][] calcularCostosMinimos(int[][] T) {
+        int n = T.length;
+        int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    C[i][j] = 0;
+                } else if (i < j) {
+                    C[i][j] = T[i][j];
+                } else {
+                    C[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    if (T[i][k] != Integer.MAX_VALUE && C[k][j] != Integer.MAX_VALUE) {
+                        int costoConEscala = T[i][k] + C[k][j];
+                        C[i][j] = Math.min(C[i][j], costoConEscala);
+                    }
+                }
+            }
+        }
+        return C;
+    }
+    
+}
