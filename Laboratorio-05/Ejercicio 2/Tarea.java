8db3a7c363d70d1c64b4c19a72c4316196b19776
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-05-08 20:11:05 -0500

    Agregar métodos getter y setter en la clase Tarea
    Se añadieron los métodos setTitulo, setPrioridad, getTitulo y getPrioridad en la clase Tarea para permitir el acceso y modificación de los atributos titulo y prioridad.

--- Laboratorio-05/Ejercicio 2/Tarea.java
+++ Laboratorio-05/Ejercicio 2/Tarea.java
@@ -3,5 +3,19 @@ public class Tarea{
     private String titulo;
     private int prioridad;
 
+    private void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
     
+    private void setPrioridad(int prioridad){
+        this.prioridad = prioridad;
+    }
+
+    private String getTitulo(){
+        return this.titulo;
+    }
+
+    private int getPrioridad(){
+        return this.prioridad;
+    }
 }
