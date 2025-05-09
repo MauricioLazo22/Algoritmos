76ffa0886defad9e2cfe7c0cdbc62af1a5cb2e8c
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-05-08 19:40:05 -0500

    Agregar método para verificar si la lista está vacía
    Se implementó el método `laListaEsVacia()` en la clase `ListaEnlazada`, el cual verifica si la lista está vacía comprobando si el primer nodo no tiene un siguiente nodo. Retorna `true` si la lista está vacía, y `false` en caso contrario.

--- Laboratorio-05/Ejercicio1/ListaEnlazada.java
+++ Laboratorio-05/Ejercicio1/ListaEnlazada.java
@@ -1,7 +1,16 @@
 public class ListaEnlazada<T> {
+    
 	private Nodaso<?> primerito;
 	
 	public ListaEnlazada(Nodaso<?> primerito) {
 		this.primerito = primerito;
+	}
+
+    public boolean laListaEsVacia() {
+		if(primerito.getElQueSigue() == null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
