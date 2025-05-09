87de589b20d5c01a1427173cb141d24bb3e54f5c
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-05-08 20:42:33 -0500

    Agregar tareas a la lista de tareas
    Se agregan tres tareas a la lista de tareas utilizando el método agregarTarea del GestorDeTareas.

--- Laboratorio-05/Actividad 2/Main.java
+++ Laboratorio-05/Actividad 2/Main.java
@@ -3,6 +3,16 @@ public class Main {
         // Paso 1: Crear una instancia de GestorDeTareas
         ListaEnlazada<Tarea> listaDeTareas = new ListaEnlazada<>(null); 
         GestorDeTareas<Tarea> gestor = new GestorDeTareas<>(listaDeTareas);
+
+        // Paso 2: Agregar tareas
+        Tarea tarea1 = new Tarea("Comprar leche", 2);
+        Tarea tarea2 = new Tarea("Estudiar Java", 1);
+        Tarea tarea3 = new Tarea("Hacer ejercicio", 3);
+
+        gestor.agregarTarea(tarea1);
+        gestor.agregarTarea(tarea2);
+        gestor.agregarTarea(tarea3);
+
         
     }
 }
