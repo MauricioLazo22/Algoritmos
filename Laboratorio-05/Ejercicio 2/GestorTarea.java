7d52615e557ef6c0f8da61eea630a07525df3251
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-05-08 20:15:02 -0500

    Agregar métodos para agregar y eliminar tareas en GestorDeTareas
    Se añadieron los métodos agregarTarea y eliminarTarea a la clase GestorDeTareas. agregarTarea inserta una tarea al final de la lista, mientras que eliminarTarea elimina una tarea si existe y retorna un valor booleano indicando el éxito de la operación.

--- Laboratorio-05/Ejercicio 2/GestorTarea.java
+++ Laboratorio-05/Ejercicio 2/GestorTarea.java
@@ -4,4 +4,19 @@ public class GestorDeTareas<T> {
     public GestorDeTareas(ListaEnlazada<T> lista) {
         this.lista = lista;
     }
-}
+
+    public void agregarTarea(T tarea) {
+        lista.insertarUltimo(tarea);
+    }
+
+    public boolean eliminarTarea(T tarea) {
+        int posicion = lista.buscar(tarea);
+        if (posicion != -1) {
+            lista.borrarNodo(tarea);
+            return true;
+        }
+        return false;
+    }
+
+    
+}   
