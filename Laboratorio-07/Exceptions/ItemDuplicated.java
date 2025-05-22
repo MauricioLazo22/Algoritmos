2c10991359bcbbf4f1d96555bf4f85c3d21bbcf9
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-05-21 23:38:11 -0500

    Agregar excepciones ItemDuplicated y ExceptionIsEmpty para manejo de errores específicos
    Se crean dos clases de excepción personalizadas ItemDuplicated para controlar la duplicación de ítems y ExceptionIsEmpty para manejar casos en que una colección o dato esté vacío. Ambas extienden Exception y permiten enviar mensajes personalizados al lanzar la excepción.

---
+++ Laboratorio-07/Exceptions/ItemDuplicated.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class ItemDuplicated extends Exception{
+
+    public ItemDuplicated(String msg){
+        super(msg);
+    }
+
+    public ItemDuplicated(){
+        super();
+    }
+}
