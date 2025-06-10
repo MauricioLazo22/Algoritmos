05ce04be8082e360e91b28d10e1217368b40842e
Arpasi Machaca Alex <112656225+Alxx1@users.noreply.github.com>
2025-06-10 15:42:21 -0500

    Implementa el método insertEdge en GraphLink
    Se implementa el método insertEdge en la clase GraphLink. Este método busca los vértices de origen y destino en la lista de vértices del grafo y, si ambos vértices existen, crea un borde entre ellos e inserta el borde en la lista de adyacencia del vértice de origen.

--- Laboratorio-09/graph/GraphLink.java
+++ Laboratorio-09/graph/GraphLink.java
@@ -13,7 +13,24 @@ public class GraphLink<E> {
     }
 
     public void insertEdge(E verOri, E verDes) {
+        Vertex<E> origen = null;
+        Vertex<E> destino = null;
         
+        for (Vertex<E> v : listVertex) {
+            if (v.getData().equals(verOri)) {
+                origen = v;
+            }
+            if (v.getData().equals(verDes)) {
+                destino = v;
+            }
+        }
+
+        if (origen != null && destino != null) {
+            Edge<E> edge = new Edge<>(destino);
+            origen.listAdj.insert(edge);
+        } else {
+            System.out.println("Vértice no encontrado.");
+        }
     }
 
     public String toString() {
