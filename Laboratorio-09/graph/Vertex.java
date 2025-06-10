1612e1af90a8a6688c15ead9778e9fb00df64a37
Arpasi Machaca Alex <112656225+Alxx1@users.noreply.github.com>
2025-06-10 15:24:43 -0500

    Agrega método getData y sobrecarga equals en la clase Vertex
    Se agrega el método getData para acceder al dato almacenado en el vértice. Además, se sobrecarga el método equals para comparar dos objetos Vertex, verificando si sus datos son iguales.

--- Laboratorio-09/graph/Vertex.java
+++ Laboratorio-09/graph/Vertex.java
@@ -9,4 +9,16 @@ public class Vertex {
         listAdj = new ListLinked<Edge<E>>();
     }
 
+    public E getData(){
+        return data;
+    }
+
+    public boolean equals(Object o){
+        if (o instanceof Vertex<?>){
+            Vertex<E> v = (Vertex<E>) o;
+            return this.data.equals(v.data);
+        }
+        return false;
+    }
+
 }
