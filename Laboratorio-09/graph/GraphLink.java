17a1f0fdf9aeaffa1dafdd8a246a4fc2517a64b9
Arpasi Machaca Alex <112656225+Alxx1@users.noreply.github.com>
2025-06-10 15:38:42 -0500

    Agrega clase GraphLink con lista de vértices y métodos de inserción sin implementar
    Se implementa la clase GraphLink, que almacena una lista de vértices en un grafo. Se agrega el método insertVertex para insertar vértices (aún sin implementación) y el método insertEdge para insertar bordes entre vértices (también sin implementación). Además, se sobrescribe el método toString para retornar una representación en cadena de la lista de vértices.

--- Laboratorio-09/graph/GraphLink.java
+++ Laboratorio-09/graph/GraphLink.java
@@ -1,5 +1,22 @@
 package Laboratorio-09.graph;
 
 public class GraphLink<E> {
+    protected ListLinked<Vertex<E>> listVertex;
+
+    public GraphLink() {
+        listVertex = new ListLinked<Vertex<E>>();
+    }
+
+    public void insertVertex(E data) {
+        
+    }
+
+    public void insertEdge(E verOri, E verDes) {
+        
+    }
+
+    public String toString() {
+        return this.listVertex.toString();
+    }
 
 }
