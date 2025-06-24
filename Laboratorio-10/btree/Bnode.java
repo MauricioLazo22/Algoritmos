2a90a4c4fb402e47469806471e68cd9089a85e8c
Alexander1909 <lucho110901@gmail.com>
2025-06-24 16:16:05 -0500

    Agrega estructura inicial de la clase BNode para árbol B genérico
    Se define la clase BNode con listas de claves y nodos hijos incluyendo el constructor y las firmas de métodos para verificar si el nodo está lleno o vacío realizar búsqueda y mostrar contenido pero aún sin implementar la lógica de dichos métodos.

---
+++ Laboratorio-10/btree/Bnode.java
@@ -0,0 +1,33 @@
+package Laboratorio-10.btree;
+
+public class BNode<E> {
+    protected ArrayList<E> keys;
+    protected ArrayList<BNode<E>> childs;
+    protected int count;
+
+    public BNode (int n){
+        this.keys = new ArrayList<E>(n);
+        this.childs = new ArrayList<BNode<E>>(n);
+        this.count = 0;
+        for(int i=0; i < n;
+            i++){ this.keys.add(null);
+            this.childs.add(null);
+        }
+    }
+    //Check if the current node is full
+    public boolean nodeFull () {
+    }
+    //Check if the current node is empty
+    public boolean nodeEmpty () {
+
+    }
+    
+    //Search for a key in the current node, if found it returns true and
+    //the position where it is located, otherwise, returns false and the
+    //position of the child where it should descend.
+    public boolean searchNode ( ) {
+    }
+    //Return the keys found in the node.
+    public String toString(){
+    }
+}
